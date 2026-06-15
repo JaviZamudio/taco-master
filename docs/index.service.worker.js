b6b8bbf36adc4d10ab7bde0d3575dd360e6a038a
JaviZamudio <javier.elihu100@gmail.com>
2026-06-14 22:58:49 -0600

    CHORE: changed config to make pwa landscape and fullscreen

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781487616|6760892338';
+const CACHE_VERSION = '1781498872|18016431966';
 /** @type {string} */
 const CACHE_PREFIX = 'TacoMaster-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
